1b604e7241274cb1255494adab7c2c8fdb78565b
Yana <sieghostienko@gmail.com>
2023-04-24 12:24:59 +0300

    условие задачи 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,3 +4,16 @@
 // a = 2,  b = 10 -> нет
 // a = 9,  b = -3 -> да
 // a = -3, b = 9 -> нет
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+ if(a == b * b)
+ {
+    Console.WriteLine("Первое число является квадратом второго");
+ }
+ else
+ {
+   Console.WriteLine("Первое число не является квадратом второго");
+ }
