b0f6a38f22c0a650c37fdf902fd49624fbba2093
Yana <sieghostienko@gmail.com>
2023-04-24 12:26:03 +0300

    решение задачи 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,7 +3,7 @@
 // a = 25, b = 5 -> да
 // a = 2,  b = 10 -> нет
 // a = 9,  b = -3 -> да
-// a = -3, b = 9 -> нет
+// a = -3, b = 9 -> нет.
 
 Console.WriteLine("Введите первое число:");
 int a = Convert.ToInt32(Console.ReadLine());
